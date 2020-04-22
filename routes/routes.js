b0feae0ba4634954576aaff841d39ce2e3a86481
Lane Enget <lane.enget@go.minneapolis.edu>
2020-04-22 16:18:18 -0500

    hello world api

---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+let express = require('express')
+let router = express.Router()
+
+router.get('/', function(req, res, next){
+    res.json({'message': 'Hello world!'})
+})
+
+module.exports = router
